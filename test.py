c9940fbbb41980e56daaff4af86c107beeb7c14d
Adam J. Penn <madannep@gmail.com>
2017-03-01 00:37:54 -0800

    removed unnessasary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from appJar import gui
-def get(btn):
-    print(app.getOptionBox("Favourite Pets"))
-
-app=gui()
-app.setFont(20)
-app.addTickOptionBox("Favourite Pets", ["Dogs", "Cats", "Hamsters", "Fish"])
-app.addButton("GET", get)
-app.go()
