1df429161df7e4340dc687643eb0efedab994e05
Adam J. Penn <madannep@gmail.com>
2017-04-30 21:03:14 -0700

    updated sending to split into string

--- server.py
+++ server.py
@@ -6,12 +6,13 @@ hostMACAddress = 'b8:27:eb:2d:17:5a'
 #hostMACAddress = 'b8:27:eb:7b:60:24'
 port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
 backlog = 1
-size = 1024
+size = 4096
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.bind((hostMACAddress,port))
 s.listen(backlog)
 try:
     client, address = s.accept()
+    print('Connected by', address)
     while 1:
         data = client.recv(size)
         if data:
