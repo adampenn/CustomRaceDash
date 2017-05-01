ac0ede5b6bfe28153cf1c8ef7dd9b1c50e95c40e
Adam J. Penn <madannep@gmail.com>
2017-05-01 12:45:26 -0700

    added some tests for layout and styling

--- test2.py
+++ test2.py
@@ -1,15 +1,49 @@
-from tkinter import *
+#!/usr/bin/python3
+
+# System modules
+import tkinter as tk
+import serial
+import socket
+import time
 from tkinter import ttk
+from queue import Queue
+from threading import Thread
 
-def main():
-    root = Tk()
-    s = ttk.Style()
-    s.theme_use("default")
-    s.configure("TProgressbar", thickness=50)
-    pb = ttk.Progressbar(root, orient="vertical", style="TProgressbar", value=50, maximum=100, length=800)
-    pb.pack()
-    slider = ttk.Scale(root, from_=0, to=100, length=800, orient="horizontal")
-    slider.pack()
-    root.mainloop()
+def updateGauges(app):
+  print("*** Update thread running")
+  while True:
+    time.sleep(.05)
+    value = app.slider.get()
+    app.pb["value"] = value
+    if value > 7500:
+      app.style.configure("TProgressbar", background='red', forground='red', thickness=100)
+    elif value > 5000:
+      app.style.configure("TProgressbar", background='yellow', forground='yellow', thickness=100)
+    else:
+      app.style.configure("TProgressbar", background='white', forground='white', thickness=100)
 
-main()
+class main(tk.Tk):
+  def __init__(self):
+    # Init
+    tk.Tk.__init__(self)
+    self.style = ttk.Style()
+    self.style.configure("TProgressbar", background='red', forground='red', thickness=100)
+    self.geometry('800x480')
+    #s.theme_use("calm")
+    self.pb = ttk.Progressbar(self, orient="vertical", style="TProgressbar", value=50, maximum=8000, length=450)
+    self.pb.grid(row=1, sticky='W')
+    self.slider = ttk.Scale(self, from_=0, to=8000, length=400, orient="horizontal")
+    self.slider.grid(row=2, column=0)
+
+print('*** Main thread running')
+
+app = main()
+
+update = Thread(target=updateGauges, args=(app,))
+update.setDaemon(True)
+update.start()
+
+app.mainloop()
+
+queue.join()
+print('*** Done')
--- test3.py
+++ test3.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-frame = tk.Frame(root)
+def main():
+  root = tk.Tk()
+  frame = tk.Frame(root)
   frame.grid()
   s = ttk.Style()
   s.theme_use('clam')
@@ -11,59 +13,6 @@ frame = tk.Frame(root)
   frame.pack()
   root.mainloop()
 
-
-def main():
-    # Init
-    tk.Tk.__init__(self)
-    style = ttk.Style()
-    style.theme_use("calm")
-    style.configure("RPMhigh", forground='red', thickness=100)
-    style.configure("RPMreg", forground='blue', thickness=100)
-    style.configure("BW.TLabel", foreground="black")
-    self.geometry('800x480')
-
-    # Set up the different screens
-    notebook = ttk.Notebook(self)
-    screen1 = ttk.Frame(notebook)
-    screen2 = ttk.Frame(notebook)
-    notebook.add(screen1, text='Screen 1')
-    notebook.add(screen2, text='Screen 2')
-    notebook.pack()
-
-    # TEMP
-    self.labelTemp = ttk.Label(screen1, text="Engine Temp C", style="BW.TLabel")
-    self.labelTemp.pack()
-    self.Temp = ttk.Progressbar(screen1, orient="horizontal", length=800,
-                               mode="determinate", value=0, maximum=250)
-    self.Temp.pack()
-
-    # RPM
-    self.labelRPM = ttk.Label(screen1, text="RPM", style="BW.TLabel")
-    self.labelRPM.pack()
-    self.RPM = ttk.Progressbar(screen1, orient="vertical", length=800,
-                               mode="determinate", value=0, maximum=7000)
-    self.RPM.pack()
-
-    # MPH
-    self.labelMPH = ttk.Label(screen2, text="MPH", style="BW.TLabel")
-    self.labelMPH.pack()
-    self.MPH = ttk.Progressbar(screen2, orient="horizontal", length=800,
-                               mode="determinate", value=0, maximum=120)
-    self.MPH.pack()
-
-    # Fuel Level
-    self.labelFuelLevel = ttk.Label(screen2, text="Fuel Level", style="BW.TLabel")
-    self.labelFuelLevel.pack()
-    self.fuelLevel = ttk.Progressbar(screen2, orient="horizontal", length=800,
-                               mode="determinate", value=0, maximum=100)
-    self.fuelLevel.pack()
-
-    # Intake Temp
-    self.labelIntakeTemp = ttk.Label(screen2, text="Intake Temp", style="BW.TLabel")
-    self.labelIntakeTemp.pack()
-    self.intakeTemp = ttk.Progressbar(screen2, orient="horizontal", length=800,
-                               mode="determinate", value=0, maximum=200)
-    self.intakeTemp.pack()
 
 main()
 
