78ac4902cf1f18f70e3a3cf4c2b86e5b995b2af5
Adam J. Penn <madannep@gmail.com>
2017-04-30 23:06:37 -0700

    pushing server file and switching server after reboot

--- server.py
+++ server.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3/3
 
 import socket
+import pickle
 
-hostMACAddress = 'b8:27:eb:2d:17:5a'
-#hostMACAddress = 'b8:27:eb:7b:60:24'
+#hostMACAddress = 'b8:27:eb:2d:17:5a'
+hostMACAddress = 'b8:27:eb:7b:60:24'
 port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
 backlog = 1
 size = 4096
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.bind((hostMACAddress,port))
 s.listen(backlog)
-try:
-    client, address = s.accept()
-    print('Connected by', address)
-    while 1:
-        data = client.recv(size)
-        if data:
-            data_arr = pickle.dumps(data)
-            print(data_arr)
-            client.send(data_arr)
-except:	
-    print("Closing socket")	
-    client.close()
-    s.close()
+client, address = s.accept()
+print('Connected by', address)
+while 1:
+  data = client.recv(size)
+  if data:
+    print(data.decode("utf-8"))
+    client.send(data)
+
+print("Closing socket")	
+client.close()
+s.close()
